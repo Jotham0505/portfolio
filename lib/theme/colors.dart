014797b98bb3d31a6728b0fd0d168c254fab6e48
Jotham Emmanuel Cheeran <jothamemmanuelcheeran@gmail.com>
2025-08-13 22:37:49 +0530

    assets and fonts

---
+++ lib/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF222222);
+  static const Color secondary = Color(0xFF7B7B7B);
+  static const Color tertiary = Color(0xFFF8F8F8);
+  static const Color white = Color(0xFFFFFFFF);
+}
---
+++ lib/theme/typography.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTextStyles {
+  static const TextStyle heading = TextStyle(
+    fontFamily: 'Aeonik',
+    fontSize: 48,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+
+  static const TextStyle body = TextStyle(
+    fontFamily: 'Aeonik',
+    fontSize: 16,
+    color: AppColors.secondary,
+  );
+}
